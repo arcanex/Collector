a83e85c559bfcaea0b3d0944c1b77a1f632a8c88
arcanexx <Scott.J.Anderson81@gmail.com>
2014-11-15 17:13:55 -0700

    push historical quotes to DB

---
+++ Collector/DatabaseSeeder/Models/HistoricalQuote.cs
@@ -0,0 +1,16 @@
+﻿namespace DatabaseSeeder.Models
+{
+	internal static class HistoricalQuoteTable
+	{
+		public const string Symbol = "symbol";
+		public const string Date = "date";
+		public const string Open = "open";
+		public const string High = "high";
+		public const string Low = "low";
+		public const string Close = "close";
+		public const string Volume = "volume";
+		public const string AdjClose = "adjclose";
+	}
+}
+
+
